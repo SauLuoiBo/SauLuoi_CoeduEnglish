c2b2d880e78c68ead1c33b289474af36b02d4809
SauLuoi <dai.dotien@gmail.com>
2022-04-04 01:47:03 +0700

    fix 2

--- client/src/Section/CourseSection/Components/index.js
+++
@@ -1,4 +0,0 @@
-import { ImgContent } from "./ImgContent";
-import { VideoContent } from "./VideoContent";
-
-export { ImgContent, VideoContent };
--- client/src/Section/SupportSection/index.js
+++ client/src/Section/SupportSection/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import * as Sui from "../../components";
-import * as Co from "./components";
 
 export const SupportSection = () => {
   return (
