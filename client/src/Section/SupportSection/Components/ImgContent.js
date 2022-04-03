471943252442e9f97e45dd977285d79e04df8009
SauLuoi <dai.dotien@gmail.com>
2022-04-04 01:45:08 +0700

    fix

---
+++ client/src/Section/SupportSection/Components/ImgContent.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { getImage } from "../../../Functions";
+import { motion } from "framer-motion";
+
+export const ImgContent = () => {
+  return (
+    <Style.Wrapper>
+      <motion.div
+        initial={{ y: 0 }}
+        animate={{ y: [-50, 0, 50] }}
+        transition={{ duration: 3, times: [0, 0.2, 1], yoyo: Infinity }}
+      >
+        <Style.Img src={getImage.getPng("balloon")} />
+      </motion.div>
+    </Style.Wrapper>
+  );
+};
+
+const Style = {
+  Wrapper: styled.div`
+    position: absolute;
+    top: 0;
+    z-index: -1;
+    right: 0;
+    width: 400px;
+  `,
+
+  Img: styled.img`
+    width: 100%;
+  `,
+};
